60ffd870a20779ae551b33e889f826f618215a01
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-07-02 22:09:17 +0000

    Add uncensoredAi function to fetch data from API

---
+++ index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import fetch from  'node-fetch';
+
+const route = "https://panel-cyphers.nett.to/cyphers"
+
+async function uncensoredAi(prompt){
+    const request = await fetch(`${route}/veniceUncensored?prompt=${encodeURIComponent(prompt)}`);
+    const response = await request.json();
+     console.log(response)
