137d0a9432795d313dccd3a51b46dd3441c89717
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-07-07 12:10:39 +0000

    Add axios import and multiple async functions

--- _otherFunc/oddFuncs.js
+++ _otherFunc/oddFuncs.js
@@ -1,4 +1,6 @@
 import crypto from 'crypto';
+import axios from 'axios';
+
 
 function failed(err){
         try{
@@ -69,9 +71,66 @@ function decrypt(KEY,IV,DATA){
   let decrypted = decipher.update(DATA,"hex","utf8");
       decrypted += decipher.final("utf8");
 
+      return { data : decrypted };
       
-      return `\n\x1b[38mThe decrypted value is\x1b[0m $$ \x1b[1;32m${decrypted}\x1b[0m`;
 }catch(err){ failed(err)}
+
+}
+
+
+
+
+
+const tiktokStalk =async(user) =>{
+    console.log(`\x1b[35m NOTE : no information is generated at times if that user account is private for privacy reasons...\x1b[0m`);
+       if(!user){
+          return logErr("The username is required to get an accurate information about that users tiktok account legally...") 
+
+       };
+    
+    const request = await axios.get(`https://panel-cyphers.nett.to/cyphers/tiktokStalk?user=${ user }`);
+    const response = request.data;
+      return  { data :response };
+}
+
+
+
+const apk = async(name) =>{
+  if(!name){
+   return logErr(`The apk name to fetch is required...`);
+} 
+    const request = await fetch(`https://panel-cyphers.nett.to/cyphers/apk?name=${name}`);
+     const response = await request.json();
+   
+    return  { data : response };
+};
+
+
+
+
+const github = async(Url, typeOfInfo)=>{
+   if(!Url){
+    return logErr("The github url to get the information about was nt given");      
+}
+    if(typeOfInfo !== 'zip' || typeOfInfo !== 'json'){
+   return logErr('The type of info can only   "json" or "zip"')
+}
+    const info = await fetch(`https://panel-cyphers.nett.to/cyphers/github?url=${ Url}&type=${typeOfInfo}`)
+    
+    const response = await info.json();
+    
+    return  { data : response };
+    
+}
+
+
+
+
+const programming = async() =>{
+  const request = await fetch("https://panel-cyphers.nett.to/cyphers/programming",);
+    
+    const response = await request.json();
+    return  { data : response.data };
 
 }
 
@@ -81,5 +140,9 @@ export {
         failed,
     logErr,
     encrypt,
-    decrypt
+    decrypt,
+tiktokStalk,
+    apk,
+    github,
+        programming
 };
